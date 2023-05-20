e50ea9e774880fccb3fca1dadbde737b7ac182f3
Denis Sebastião Costa Barbosa <denis.barbosa@inatel.br>
2023-05-20 18:13:40 -0300

    Add db script, fix db entity

--- trabalho-ejb/src/main/java/br/inatel/trabalhodm110/entities/Payment.java
+++ trabalho-ejb/src/main/java/br/inatel/trabalhodm110/entities/Payment.java
@@ -2,6 +2,7 @@ package br.inatel.trabalhodm110.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
@@ -13,10 +14,19 @@ public class Payment implements Serializable {
 	private static final long serialVersionUID = 5938979026494446541L;
 	
 	@Id
+	@Column(name = "PRODUCT_CODE")
 	private int productCode;
+	
+	@Column(name = "PAYMENT_DATE")
 	private String paymentDate;
+	
+	@Column(name = "DUE_DATE")
 	private String dueDate;
+	
+	@Column(name = "VALUE")
 	private double value;
+	
+	@Column(name = "PAYMENT_TYPE")
 	private String paymentType;
 	
 	public int getProductCode() 
